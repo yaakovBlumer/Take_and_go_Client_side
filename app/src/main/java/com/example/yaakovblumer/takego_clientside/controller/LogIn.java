a03ff2674371ffd6c7ebe966f38be3de78f3ab50
yaakovBlumer <yyb034@gmail.com>
2018-06-04 17:50:48 +0300

    יצרתי את האקטיביטי הראשון של ה-log in. המשך מחר בעז"ה.

---
+++ app/src/main/java/com/example/yaakovblumer/takego_clientside/controller/LogIn.java
@@ -0,0 +1,15 @@
+package com.example.yaakovblumer.takego_clientside.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yaakovblumer.takego_clientside.R;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
